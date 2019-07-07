6067c7088f9a448652e2facfde378273bc93c6a6
bheck1 <bheckie@gmail.com>
2019-07-07 15:44:43 -0600

    i

--- app/move.py
+++ app/move.py
@@ -14,12 +14,11 @@ game_state = ""
 directions = {'up': 0, 'down': 0, 'left': 0, 'right': 0}
 
 
-def calculate_move(board_matrix, game_state):
-
-	if len(game["you"]["body"]) < 5:
-		HEALTHLIM = 100
-	else:
-		HEALTHLIM = 10
+def calculate_move(board_matrix, game_state):		
+    if len(game["you"]["body"]) < 5:
+        HEALTHLIM = 100
+    else:
+        HEALTHLIM = 5		
 		
     set_game_state(game_state)
     height = game_state["board"]["height"]
