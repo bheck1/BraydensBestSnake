b667dda85f220ee2add66c4f36b9620bdbf88cc1
bheck1 <bheckie@gmail.com>
2019-07-07 13:16:05 -0600

    jj

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def ping():
 @bottle.post('/start')
 def start():
     game_state = bottle.request.json
-    snake_colour = "#00FF00"
+    snake_colour = "#E14525"
     return start_response(snake_colour)
 
 
